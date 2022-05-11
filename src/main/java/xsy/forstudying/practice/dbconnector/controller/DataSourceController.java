3a029d3dcad3f19a2905b6449c0846926300ae25
xusiyu <xsy534393044@163.com>
2022-05-11 16:59:41 +0800

    小改动20220511

--- src/main/java/xsy/forstudying/practice/dbconnector/controller/DataSourceController.java
+++ src/main/java/xsy/forstudying/practice/dbconnector/controller/DataSourceController.java
@@ -1,10 +1,8 @@
 package xsy.forstudying.practice.dbconnector.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import xsy.forstudying.practice.dbconnector.model.DataSourceConfiguration;
 import xsy.forstudying.practice.dbconnector.model.DataSourceInfo;
 import xsy.forstudying.practice.dbconnector.service.IDataSourceService;
 
@@ -24,14 +22,34 @@ public class DataSourceController {
     @Autowired
     IDataSourceService dataSourceService;
 
-   @GetMapping("/connect")
+    @GetMapping("/connect")
     public List<Map<String, Object>> dbConnector(@RequestParam Long id) throws Exception {
-       List<Map<String, Object>> result = dataSourceService.getResult(id);
-       return result;
-   }
+        List<Map<String, Object>> result = dataSourceService.getResult(id);
+        return result;
+    }
 
-   @GetMapping("/test")
-    public List<DataSourceInfo> queryAllDataSourceInfo(){
-       return dataSourceService.queryAll();
-   }
+    @GetMapping("/test")
+    public List<DataSourceInfo> queryAllDataSourceInfo() {
+        return dataSourceService.queryAll();
+    }
+
+    @PostMapping("/configuration/add")
+    public String addConfiguration(DataSourceConfiguration dataSourceConfiguration) {
+        return null;
+    }
+
+    @PostMapping("/configuration/edit")
+    public String editConfiguration(DataSourceConfiguration dataSourceConfiguration) {
+        return null;
+    }
+
+    @PostMapping("/sql/add")
+    public String addSql(){
+        return null;
+    }
+
+    @PostMapping("/sql/edit")
+    public String editSql(){
+        return null;
+    }
 }
---
+++ src/main/java/xsy/forstudying/practice/dbconnector/model/DataSourceConfiguration.java
@@ -0,0 +1,17 @@
+package xsy.forstudying.practice.dbconnector.model;
+
+import lombok.Data;
+
+/**
+ * @author XuSiYu
+ * @version 1.0
+ * @date 2022-05-11 9:29
+ **/
+
+@Data
+public class DataSourceConfiguration {
+    private DataSourceInfo dataSourceInfo;
+    private String dbType;
+//    private String
+
+}
